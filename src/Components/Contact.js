1317736ce1faa18170cd05d281d4c4d59d3301ae
Durgavino <mailtodurga93@gmail.com>
2023-03-29 11:33:42 -0300

    added validation for email

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from 'react-hook-form';
 
 const Contact = () => {
     const { register, handleSubmit } = useForm();
-        const handleRegistration = (data) => console.log(data);
+    const handleRegistration = (data) => console.log(data);
+    const [email, setEmail] = useState("");
+    
+    const [message, setMessage] = useState("");
+
+    const emailvalidation = () => {
+        const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!pattern.test(email)) {
+            setMessage("Enter the Valid Email Address")
+        }
+    }
+    const handleonchange = (e) => {
+        setEmail(e.target.value);
+    };
 
     return (
-<form onSubmit={handleSubmit(handleRegistration)}>
-        <div>
-            <p>Contact Me</p>
+        <form onSubmit={handleSubmit(handleRegistration)}>
+            <div>
+                <p>Contact Me</p>
 
-            <input type='text' placeholder="Full Name" />
-            <br>
-            </br>
+                <input type='text' placeholder="Full Name" />
+                <br>
+                </br>
 
-            <input type='text' name="email" placeholder="Email Address" {...register('email')}/>
-            <br>
-            </br>
+                <input type='text' name="email" value={email} onChange={handleonchange} placeholder="Email Address" {...register('email')} />
+                <br>
+                </br>
 
-            <textarea rows="4" cols="50" name="comment" placeholder="Enter Your Message ..." {...register('email',{required:"Enter Your Message"})} >
-            </textarea>
-<br></br>
+                <textarea rows="4" cols="50" name="comment" placeholder="Enter Your Message ..." {...register('comment', { required: "Enter Your Message" })} >
+                </textarea>
+                <br></br>
 
-            <button type="submit" value="Send">Send</button>
-            <h1>The button form attribute</h1>
-        </div>
+                <button onClick={emailvalidation} type="submit" value="Send">Send</button>
+                {message}
+            </div>
         </form>
 
 
