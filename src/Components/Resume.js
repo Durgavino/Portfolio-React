ea955c8456aa83a35157aa535c4d08ae11d19b8a
Durgavino <mailtodurga93@gmail.com>
2023-03-29 17:24:07 -0300

    resume file working

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,12 +1,14 @@
 import React from "react";
-//import resume from "../assets/"
+//import resume from ""
 const Resume=()=>{
 return(
     <div>
 <button className="resume">
    
-    <a href="../assets/Durgadevi Baskaran resume-2023.pdf" download="Durgadevi Baskaran resume-2023.pdf"
-    >Download Resume</a>
+    {/* <a href="../assets/Durgadevi Baskaran resume-2023.pdf" download="Durgadevi Baskaran resume-2023.pdf"
+    >Download Resume</a> */}
+
+<a href={process.env.PUBLIC_URL + "/assets/Durgadevi Baskaran resume-2023.pdf"} download  >Download Resume</a>
 </button>
 
 
